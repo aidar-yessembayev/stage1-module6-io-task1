3bf0f3f4c69f35c5987c302ec9753e6279091220
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-17 19:12:26 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,28 @@ import java.io.File;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        // 1. Reading file data into string
+        String textFromFile = "";
+
+        try (FileInputStream in = new FileInputStream(file.getPath())) {
+
+            int ch;
+            while ((ch = in.read()) != -1) {
+                textFromFile += (char) ch;
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        // 2. Parse this string for key-value pairs
+        String[] arrOfText = textFromFile.split("\n", 4);
+
+        return new Profile(
+                arrOfText[0].substring(6),
+                Integer.parseInt(arrOfText[1].substring(5)),
+                arrOfText[2].substring(7),
+                Long.parseLong(arrOfText[3].substring(7))
+        );
     }
 }
