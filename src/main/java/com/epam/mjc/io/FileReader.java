25d18fffec572397f0320edbac034eedbc817a70
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-18 10:36:13 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        
         // 1. Reading file data into string
         String textFromFile = "";
 
@@ -34,12 +35,13 @@ public class FileReader {
             name = arrOfText[0].substring(6);
             age = Integer.parseInt(arrOfText[1].substring(5));
             email = arrOfText[2].substring(7);
-            phone = Long.parseLong(arrOfText[3].substring(7));
+            phone =  Long.valueOf(Integer.parseInt(arrOfText[3].substring(7)));
 
         } catch (NumberFormatException nfe) {
-            System.out.println("NumberFormat Exception: invalid input string");
+            nfe.printStackTrace();
         }
 
         return new Profile(name, age, email, phone);
+        
     }
 }
