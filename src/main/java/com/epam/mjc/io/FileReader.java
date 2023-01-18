5b4a50883c579039432505088288888c9d1fcead
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-18 10:49:48 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -35,7 +35,7 @@ public class FileReader {
             name = arrOfText[0].substring(6);
             age = Integer.parseInt(arrOfText[1].substring(5));
             email = arrOfText[2].substring(7);
-            phone =  Long.valueOf(Integer.parseInt(arrOfText[3].substring(7)));
+            phone =  Long.parseLong(arrOfText[3].substring(7, arrOfText[3].length() - 1));
 
         } catch (NumberFormatException nfe) {
             nfe.printStackTrace();
